aafc006aa07b018505f08a6c758223b1b98d1ab9
Raphides <89037051+Raphides@users.noreply.github.com>
2022-07-27 00:30:41 -0300

    Update and rename init.java to package/init.java

--- init.java
+++
@@ -1,9 +0,0 @@
-public class init {
-    public static void main(String[] args) {
-        CreatingFiles o = new CreatingFiles();
-        o.addLine("Raphael", "TEuCu6969", "31-02-20xx-25:63:62", false); 
-        o.addLine("Raphael", "TEuCu6969", "31-02-20xx-25:63:62", false);
-
-    }
-
-}
---
+++ package/init.java
@@ -0,0 +1,6 @@
+public class init {
+    public static void main(String[] args) {
+        SystemApp screenSys = new SystemApp();
+    }
+
+}
